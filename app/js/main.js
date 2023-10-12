ec5cf9ce80b025fa22327ab5b1261c08c5fa204a
Yakiv <example@gmail.com>
2023-10-12 15:19:39 +0300

    add blockuote and services

--- app/js/main.js
+++ app/js/main.js
@@ -19,10 +19,36 @@ function moveNavToSidebar() {
   const sidebarClose = document.querySelector(".sidebar__close");
 
   if (window.innerWidth <= 768) {
-      sidebar.insertBefore(nav, sidebarClose.nextSibling);
+    sidebar.insertBefore(nav, sidebarClose.nextSibling);
   }
 }
 
 // Вызовем функцию при загрузке страницы и при изменении размера окна
 window.addEventListener("load", moveNavToSidebar);
 window.addEventListener("resize", moveNavToSidebar);
+
+
+// card realization
+// Находим все элементы с классом services__back-btn-more
+const moreButtons = document.querySelectorAll('.services__back-btn-more');
+
+// Находим все элементы с классом services__back-btn-back
+const backButtons = document.querySelectorAll('.services__back-btn-back');
+
+// Добавляем обработчики событий для кнопок "Подробнее"
+moreButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Находим родительский элемент с классом services__card и добавляем класс changed
+    const card = button.closest('.services__card');
+    card.classList.add('changed');
+  });
+});
+
+// Добавляем обработчики событий для кнопок "Назад"
+backButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Находим родительский элемент с классом services__card и удаляем класс changed
+    const card = button.closest('.services__card');
+    card.classList.remove('changed');
+  });
+});
